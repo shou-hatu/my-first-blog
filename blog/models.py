aa95338e60b355af30555a95e6623bd35641f6e2
Your Name <you@example.com>
2024-03-26 20:09:45 +0900

    コメント削除

--- blog/models.py
+++ blog/models.py
@@ -4,19 +4,16 @@ from django.utils import timezone
 
 
 """models.Model はポストがDjango Modelだという意味"""
+
+
 class Post(models.Model):
-    
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    """文字数が制限されたテキストを定義するフィールド"""
     title = models.CharField(max_length=200)
-    """これは制限無しの長いテキスト用"""
     text = models.TextField()
-    
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-"""第1引数はselfにするルール"""
+
     def publish(self):
-        """published_dateにtimezone.now()を渡す"""
         self.published_date = timezone.now()
         self.save()
 
